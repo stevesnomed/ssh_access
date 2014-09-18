4d0ce58907eb849edeb94d113dc95cc7b65d3c63
Rory Davidson <rory.davidson@gmail.com>
2014-09-18 15:16:25 +0100

    Update create_rda_user_with_sudo_access.sh

--- create_rda_user_with_sudo_access.sh
+++ create_rda_user_with_sudo_access.sh
@@ -4,7 +4,7 @@ MY_SHELL=$( which bash )
 
 /usr/sbin/useradd -c 'Rory Davidson <rda at ihtsdo.org>' -s ${MY_SHELL} -m rda
 cat >/etc/sudoers.d/rda-ALL <<EOF
-kaik     ALL=(ALL:ALL) NOPASSWD: ALL
+rda     ALL=(ALL:ALL) NOPASSWD: ALL
 EOF
 chmod 0440 /etc/sudoers.d/rda-ALL
 mkdir ~rda/.ssh
