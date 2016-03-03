b0b87427d7345f456ed1b9893ad835eebe870566
tshird <tsh@ihtsdo.org>
2016-03-03 10:55:18 +0000

    Update create_rda_user_with_sudo_access.sh
    Replaced Rory's details with mine

--- create_rda_user_with_sudo_access.sh
+++ create_rda_user_with_sudo_access.sh
@@ -2,13 +2,13 @@
 
 MY_SHELL=$( which bash )
 
-/usr/sbin/useradd -c 'Rory Davidson <rda at ihtsdo.org>' -s ${MY_SHELL} -m rda
-cat >/etc/sudoers.d/rda-ALL <<EOF
+/usr/sbin/useradd -c 'Terance Shird <tsh at ihtsdo.org>' -s ${MY_SHELL} -m tsh
+cat >/etc/sudoers.d/tsh-ALL <<EOF
 rda     ALL=(ALL:ALL) NOPASSWD: ALL
 EOF
-chmod 0440 /etc/sudoers.d/rda-ALL
-mkdir ~rda/.ssh
-chmod 700 ~rda/.ssh
-curl https://raw.githubusercontent.com/rorydavidson/ssh_access/master/rorydavidson_rsa.pub >~rda/.ssh/authorized_keys
-chmod 400 ~rda/.ssh/authorized_keys
-chown -R rda ~rda/.ssh
+chmod 0440 /etc/sudoers.d/tsh-ALL
+mkdir ~tsh/.ssh
+chmod 700 ~tsh/.ssh
+curl https://raw.githubusercontent.com/tshird/ssh_access/master/tshird_rsa.pub >~tsh/.ssh/authorized_keys
+chmod 400 ~tsh/.ssh/authorized_keys
+chown -R tsh ~tsh/.ssh
