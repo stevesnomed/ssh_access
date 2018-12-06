47e2aea2f41dc1cbb17b17c18b43d31ef7b210e3
stevesnomed <45640321+stevesnomed@users.noreply.github.com>
2018-12-06 12:08:47 +0000

    Update and rename create_tsh_user_with_sudo_access.sh to create_sallen_user_with_sudo_access.sh

---
+++ create_sallen_user_with_sudo_access.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+MY_SHELL=$( which bash )
+
+/usr/sbin/useradd -c 'Steve Allen <sal at ihtsdo.org>' -s ${MY_SHELL} -m sallen
+cat >/etc/sudoers.d/sallen-ALL <<EOF
+sallen     ALL=(ALL:ALL) NOPASSWD: ALL
+EOF
+chmod 0440 /etc/sudoers.d/sallen-ALL
+mkdir ~sallen/.ssh
+chmod 700 ~sallen/.ssh
+curl https://raw.githubusercontent.com/stevesnomed/ssh_access/master/sallen_rsa.pub >~sallen/.ssh/authorized_keys
+chmod 400 ~sallen/.ssh/authorized_keys
+chown -R sallen ~sallen/.ssh
